a659662213cfa34d63726a18af8b3c48e00b639e
Daniel Whiffing <danielwhiffing@gmail.com>
2020-04-11 16:10:59 -0400

    Adds bot

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,6 @@
 import { Missiles } from '../sprites/Missiles'
 import { Mines } from '../sprites/Mines'
+import { Bot } from '../sprites/Bot'
 
 export default class extends Phaser.Scene {
   constructor() {
@@ -7,7 +8,10 @@ export default class extends Phaser.Scene {
   }
 
   create() {
+    const { width, height } = this.game.canvas
     this.behavior = this.plugins.get('BehaviorPlugin')
+    const bot = new Bot(this, width / 2, height / 2)
+    this.bot = this.add.existing(bot)
     this.mines = new Mines(this)
     this.missileGroup = new Missiles(this)
 
