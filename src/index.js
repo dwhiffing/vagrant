6ac519a925c5ca360a4478f0c2bf7f76aa9ccc5a
Daniel Whiffing <danielwhiffing@gmail.com>
2020-04-18 12:14:47 -0400

    Fixes scaling

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import Phaser from 'phaser'
 import BehaviorPlugin from './behavior'
 import * as scenes from './scenes'
 
+const { clientWidth, clientHeight } = window.document.documentElement
+
+const isDesktop = clientWidth > 1200 && clientHeight > 800
+
 const game = new Phaser.Game({
   transparent: true,
   type: Phaser.AUTO,
   parent: 'phaser-example',
   backgroundColor: '#fff',
-  width: window.document.documentElement.clientWidth,
-  height: window.document.documentElement.clientHeight,
+  width: isDesktop ? 1920 : 1080,
+  height: 1920,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   plugins: {
     global: [{ key: 'BehaviorPlugin', plugin: BehaviorPlugin, start: true }],
   },
