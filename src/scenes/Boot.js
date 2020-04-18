eace54091bca491e0d4b522253f75cf64d951cbb
Daniel Whiffing <danielwhiffing@gmail.com>
2020-04-17 20:08:22 -0400

    update frame

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -20,40 +20,40 @@ export default class extends Phaser.Scene {
       'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js',
     )
 
-    this.load.image('background', '/assets/images/background2.gif')
-    this.load.image('bot', '/assets/images/ufoGreen.png')
-    this.load.image('mine', '/assets/images/mine.png')
-    this.load.image('target', '/assets/images/target.png')
-    this.load.image('title', '/assets/images/title.png')
-    this.load.image('smoke', '/assets/images/smoke2.png')
-    this.load.image('healthBar', '/assets/images/health-bar.png')
-    this.load.image('healthBarIn', '/assets/images/health-bar-in.png')
-    this.load.spritesheet('life', '/assets/images/life.png', {
+    this.load.image('background', 'assets/images/background2.gif')
+    this.load.image('bot', 'assets/images/ufoGreen.png')
+    this.load.image('mine', 'assets/images/mine.png')
+    this.load.image('target', 'assets/images/target.png')
+    this.load.image('title', 'assets/images/title.png')
+    this.load.image('smoke', 'assets/images/smoke2.png')
+    this.load.image('healthBar', 'assets/images/health-bar.png')
+    this.load.image('healthBarIn', 'assets/images/health-bar-in.png')
+    this.load.spritesheet('life', 'assets/images/life.png', {
       frameWidth: 25,
       frameHeight: 25,
     })
 
-    this.load.spritesheet('missile', '/assets/images/missile.png', {
+    this.load.spritesheet('missile', 'assets/images/missile.png', {
       frameWidth: 100,
       frameHeight: 100,
     })
 
-    this.load.spritesheet('explosion', '/assets/images/explosion.png', {
+    this.load.spritesheet('explosion', 'assets/images/explosion.png', {
       frameWidth: 128,
       frameHeight: 128,
     })
 
-    this.load.spritesheet('explosion-2', '/assets/images/explosion-2.png', {
+    this.load.spritesheet('explosion-2', 'assets/images/explosion-2.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
 
-    this.load.spritesheet('explosion-3', '/assets/images/explosion-3.png', {
+    this.load.spritesheet('explosion-3', 'assets/images/explosion-3.png', {
       frameWidth: 48,
       frameHeight: 48,
     })
 
-    this.load.spritesheet('rock', '/assets/images/rock.png', {
+    this.load.spritesheet('rock', 'assets/images/rock.png', {
       frameWidth: 100,
       frameHeight: 100,
     })
